68d3fc5f498294ef2ed6bcc029e0c5bbf815e445
kevin00000000 <jiannan_yang@163.com>
2018-02-02 17:04:51 +0800

    merge_into and new_int

--- sorting_searching/new_int/test_new_int.py
+++ sorting_searching/new_int/test_new_int.py
@@ -1,5 +1,20 @@
 from nose.tools import assert_equal, assert_raises
+from bitstring import BitArray  # Run pip install bitstring
 
+
+class Bits(object):
+
+    def new_int(self, array, max_size):
+        if not array:
+            raise TypeError('array cannot be None or empty')
+        base = max_size-32
+        bit_vector = BitArray(32)
+        for item in array:
+            bit_vector[item-base] = True
+        for index, item in enumerate(bit_vector):
+            if not item:
+                return index+base
+        return None
 
 class TestBits(object):
 
@@ -13,6 +28,9 @@ class TestBits(object):
         assert_equal(bits.new_int(data, max_size), 30)
         data = [item for item in range(32)]
         assert_equal(bits.new_int(data, max_size), None)
+        data = [item for item in range(1, 31)]
+        data.append(32)
+        assert_equal(bits.new_int(data, 33), 31)
         print('Success: test_find_int_excluded_from_input')
 
 
