6d09c179699bf6b350184b11507722c7384f6e1d
kevin00000000 <jiannan_yang@163.com>
2018-03-06 15:19:45 +0800

    graph_tree checkbalance bstinvert

--- graphs_trees/check_balance/test_check_balance.py
+++ graphs_trees/check_balance/test_check_balance.py
@@ -1,6 +1,60 @@
 from nose.tools import assert_equal
 from nose.tools import raises
 
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class Bst(object):
+    def __init__(self, root=None):
+        self._root = root
+
+    @property
+    def root(self):
+        return self._root
+
+    def insert(self, data):
+        if data is None:
+            raise TypeError
+        node = Node(data)
+        self._root = self._insert(node, self._root)
+        return node
+    
+    def _insert(self, node, root):
+        if root is None:
+            root = node
+        elif root.data > node.data:
+            root.left = self._insert(node, root.left)
+        else:
+            root.right = self._insert(node, root.right)
+        return root
+
+class BstBalance(Bst):
+    def __init__(self, root):
+        super().__init__(root)
+    
+    def check_balance(self):
+        if self.root is None:
+            raise TypeError("Can't check empty tree")
+        height = self._check_balance(self.root)
+        return height != -1
+
+    def _check_balance(self, node):
+        if node is None:
+            return 0
+        height_left = self._check_balance(node.left)
+        if height_left == -1:
+            return -1
+        height_right = self._check_balance(node.right)
+        if height_right == -1:
+            return -1
+        diff = abs(height_right-height_left)
+        if diff > 1:
+            return -1
+        height = 1 + max(height_left, height_right)
+        return height
 
 class TestCheckBalance(object):
 
--- graphs_trees/invert_tree/test_invert_tree.py
+++ graphs_trees/invert_tree/test_invert_tree.py
@@ -1,5 +1,51 @@
 from nose.tools import assert_equal, assert_raises
 
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class Bst(object):
+    def __init__(self, root=None):
+        self._root = root
+
+    @property
+    def root(self):
+        return self._root
+
+    def insert(self, data):
+        if data is None:
+            raise TypeError
+        node = Node(data)
+        self._root = self._insert(node, self._root)
+        return node
+    
+    def _insert(self, node, root):
+        if root is None:
+            root = node
+        elif root.data > node.data:
+            root.left = self._insert(node, root.left)
+        else:
+            root.right = self._insert(node, root.right)
+        return root
+
+class InverseBst(Bst):
+    def __init__(self, root):
+        super().__init__(root)
+    
+    def invert_tree(self):
+        if self.root is None:
+            raise TypeError("Can't invert empty tree")
+        return self._invert_tree(self.root)
+
+    def _invert_tree(self, node):
+        if node is None:
+            return None
+        node.left = self._invert_tree(node.left)
+        node.right = self._invert_tree(node.right)
+        node.left, node.right = node.right, node.left
+        return node 
 
 class TestInvertTree(object):
 
