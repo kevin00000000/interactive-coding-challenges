8a19aba2ea2d3e713bc16acc4cf0f98557def80e
kevin00000000 <jiannan_yang@163.com>
2018-04-09 17:00:02 +0800

    dynamic programming 2/3

--- recursion_dynamic/coin_change_min/test_coin_change_min.py
+++ recursion_dynamic/coin_change_min/test_coin_change_min.py
@@ -1,5 +1,37 @@
 from nose.tools import assert_equal, assert_raises
+import sys
 
+
+class CoinChanger(object):
+    def __init__(self):
+        self.result = {}
+
+    def make_change(self, coins, total):
+        if coins is None or total is None:
+            raise TypeError('coins or total is None')
+        if not len(coins) or not total:
+            return 0
+        self._init_result(len(coins), total)
+        self._fill_result(coins, len(coins), total)
+        return self.result[len(coins)][total]
+    
+    def _init_result(self, n, total):
+        for _ in range(0, n+1):
+            self.result[_] = {}
+    
+    def _fill_result(self, coins, n, total):
+        for i in range(n+1):
+            for t in range(total+1):
+                if not t:
+                    self.result[i][t] = 0
+                    continue
+                if not i:
+                    self.result[i][t] = sys.maxsize
+                    continue
+                if coins[i-1] <= t:
+                    self.result[i][t] = min(self.result[i-1][t], self.result[i][t-coins[i-1]] + 1)
+                else:
+                    self.result[i][t] = self.result[i-1][t]
 
 class TestCoinChange(object):
 
--- recursion_dynamic/coin_change_ways/test_coin_change_ways.py
+++ recursion_dynamic/coin_change_ways/test_coin_change_ways.py
@@ -1,6 +1,27 @@
 from nose.tools import assert_equal
 
 
+class solution(object):
+
+    def __init__(self):
+        self.result = {}
+    
+    def make_change(self, coins, total):
+        if coins is None or total is None:
+            raise TypeError('invalid param')
+        if not coins or not total:
+            return 0
+        self._init_result(len(coins), total)
+        self._fill_result(coins, len(coins), total)
+        return self.result[len(coins)][total]
+
+    def _init_result(self, n):
+        for _ in range(n+1):
+            self.result[_] = {}
+
+    
+
+
 class Challenge(object):
 
     def test_coin_change_ways(self,solution):
