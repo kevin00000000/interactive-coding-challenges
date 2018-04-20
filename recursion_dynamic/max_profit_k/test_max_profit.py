8e9485de81c9514329b3d8b514e90cde4d874845
kevin00000000 <jiannan_yang@163.com>
2018-04-20 17:07:49 +0800

    dynamic programming 15/16

--- recursion_dynamic/max_profit_k/test_max_profit.py
+++ recursion_dynamic/max_profit_k/test_max_profit.py
@@ -1,6 +1,80 @@
 from nose.tools import assert_equal
 from nose.tools import assert_raises
 from nose.tools import assert_true
+from enum import Enum
+import sys
+
+
+class Type(Enum):
+    SELL = 0
+    BUY = 1
+
+
+class Transaction(object):
+
+    def __init__(self, type, day, price):
+        self.type = type
+        self.day = day
+        self.price = price
+
+    def __eq__(self, other):
+        return self.type == other.type and \
+            self.day == other.day and \
+            self.price == other.price
+
+    def __repr__(self):
+        return str(self.type) + ' day: ' + \
+            str(self.day) + ' price: ' + \
+            str(self.price)
+
+
+class StockTrader(object):
+
+    def __init__(self):
+        self.results = []
+        self.result_transactions = []
+
+    def find_max_profit(self, prices, k):
+        if prices is None or k is None:
+            raise TypeError("param is None")
+        if not prices or not k:
+            return []
+        self._init_result(len(prices), k)
+        self._fill_result(prices, k)
+        return self.results[len(prices)][k], self.result_transactions[len(prices)][k]
+
+    def _init_result(self, n, k):
+        for i in range(n+1):
+            self.results.append([])
+            self.result_transactions.append([])
+            for _ in range(k+1):
+                self.results[i].append(0)
+                self.result_transactions[i].append([])
+    
+    def _fill_result(self, prices, k):
+        n = len(prices)
+        for i in range(n+1):
+            for j in range(k+1):
+                if not i or not j:
+                    self.results[i][j] = 0
+                    self.result_transactions[i][j] = []
+                    continue
+                max_profit = -sys.maxsize
+                max_buy = 0
+                for m in range(i):
+                    temp_profit = prices[i-1] - prices[m] + self.results[m][j-1]
+                    if max_profit < temp_profit:
+                        max_buy = m
+                        max_profit = temp_profit
+                self.results[i][j] = max(self.results[i-1][j], max_profit)
+                if self.results[i-1][j] >= max_profit:
+                    self.results[i][j] = self.results[i-1][j]
+                    self.result_transactions[i][j].extend(self.result_transactions[i-1][j])
+                else:
+                    self.results[i][j] = max_profit
+                    self.result_transactions[i][j].extend(self.result_transactions[i-1][j])
+                    self.result_transactions[i][j].append(Transaction(Type.BUY, max_buy, prices[max_buy]))
+                    self.result_transactions[i][j].append(Transaction(Type.SELL, i-1, prices[i-1]))
 
 
 class TestMaxProfit(object):
--- recursion_dynamic/n_pairs_parentheses/test_n_pairs_parentheses.py
+++ recursion_dynamic/n_pairs_parentheses/test_n_pairs_parentheses.py
@@ -1,6 +1,42 @@
 from nose.tools import assert_equal, assert_raises
 
 
+class Parentheses(object):
+    def __init__(self):
+        self.results = []
+        self.result_counts = []
+
+    def find_pair(self, num):
+        if num is None or type(num) is not int:
+            raise TypeError("param is None or is not int")
+        if num < 0:
+            raise ValueError("num smaller than zero")
+        if num == 0:
+            return []
+        self._init_result(num)
+        self._fill_result(num)
+        return list(sorted(self.results[num]))
+    
+    def _init_result(self, num):
+        for _ in range(num+1):
+            self.results.append(set())
+            self.result_counts.append(0)
+
+    def _fill_result(self, num):
+        for i in range(1, num+1):
+            if i == 1:
+                self.result_counts[i] = 1
+                self.results[i].add("()")
+            else:
+                self.result_counts[i] = self.result_counts[i-1] * 3 - 1
+                for item in self.results[i-1]:
+                    self.results[i].add('(' + item + ')')  
+                for item in self.results[i-1]:
+                    self.results[i].add('()' + item)
+                for item in self.results[i-1]:
+                    self.results[i].add(item + '()')      
+
+
 class TestPairParentheses(object):
 
     def test_pair_parentheses(self):
